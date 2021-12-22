cb81847319f189aaa1cf1f7049f83f0a24dc5e93
akashbg3010 <akashbpura@gmail.com>
2021-12-22 23:03:44 +0530

    Modified a contact

--- src/com/bridgelabz/ContactList.java
+++ src/com/bridgelabz/ContactList.java
@@ -1,10 +1,12 @@
 package com.bridgelabz;
 
 public class ContactList {
+	
 	String firstName,lastName,address,state,email;
 	int zip;
 	long phoneNumber;
 	ContactList(String firstName,String lastName,String address,String state,int zip,long phoneNumber,String email){
+		
 		this.firstName=firstName;
 		this.lastName=lastName;
 		this.address=address;
